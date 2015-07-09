e72ef5b4eea377289882fe1ca0010c3320564b67
Phone Zaw Phyo <phonezawphyo@gmail.com>
2015-07-09 22:57:54 +0900

    =Added 2-axis-control pad

--- websocket-client/js/app/app.js
+++ websocket-client/js/app/app.js
@@ -1,5 +1,69 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('WebsocketClient',[]);
+angular.module('WebsocketClient',[])
+.directive('bbbTwoAxisControl', function() {
+  return {
+    restrict: 'A',
+    link: function (scope, $elem, attrs) {
+      var pos = {x:0,y:0};
+      var config = {
+        width: $elem.width(),
+        height: $elem.height(),
+        minOutput: -100,
+        maxOutput: 100,
+      };
+      var trigger = false;
+
+      var posXToOutput = function(x) {
+        // o = (y * (maxO-minO) / width + minO 
+        return Math.floor(x*(config.maxOutput-config.minOutput) / config.width)+config.minOutput;
+      };
+
+      var posYToOutput = function(y) {
+        // o = (y * (maxO-minO) / height + minO 
+        return Math.floor(y*(config.maxOutput-config.minOutput) / config.height)+config.minOutput;
+      };
+
+      var reset = function () {
+        pos.x = 0;
+        pos.y = 0;
+        $elem.html("(0,0)");
+        scope.resetYaw();
+        scope.resetRoll();
+        scope.resetPitch();
+        scope.$apply();
+      };
+
+      $("body").on("mouseup",function () {
+        trigger = false;
+        reset();
+      });
+
+      $elem.on("mousemove", function (e) {
+        var parentOffset = $(this).offset();
+        pos.x = posXToOutput(e.pageX - parentOffset.left);
+        pos.y = posYToOutput(e.pageY - parentOffset.top);
+        $elem.html("("+pos.x+","+pos.y+")");
+        if (trigger) {
+          scope.sliderYaw = pos.x;
+          scope.sliderPitch = pos.y;
+          scope.$apply();
+        }
+      })
+      .on("mousedown", function () {
+        trigger = true;
+      })
+      .on("mouseup", function () {
+        trigger = false;
+        reset();
+      })
+      .on("mouseout", function () {
+        if (!trigger) {
+          reset();
+        }
+      });
+    },
+  };
+});
 
--- websocket-client/js/app/web-socket-controller.js
+++ websocket-client/js/app/web-socket-controller.js
@@ -39,16 +39,40 @@ function WebSocketController($scope) {
       ws.send("pitch:"+$scope.sliderPitch);
     };
 
+    $scope.resetYaw = resetYaw;
+    $scope.resetRoll = resetRoll;
+    $scope.resetPitch = resetPitch;
+
+    $scope.$watch("sliderYaw", function () {
+      $scope.$sliderYaw.slider("setValue",$scope.sliderYaw);
+
+      // Send the value over the WS connection.
+      if (ws.readyState === 1)
+        ws.send("yaw:"+$scope.sliderYaw);
+    });
+
+    $scope.$watch("sliderRoll", function () {
+      $scope.$sliderRoll.slider("setValue",$scope.sliderRoll);
+
+      // Send the value over the WS connection.
+      if (ws.readyState === 1)
+        ws.send("roll:"+$scope.sliderRoll);
+    });
+
+
+    $scope.$watch("sliderPitch", function () {
+      $scope.$sliderPitch.slider("setValue",$scope.sliderPitch);
+
+      // Send the value over the WS connection.
+      if (ws.readyState === 1)
+        ws.send("pitch:"+$scope.sliderPitch);
+    });
+
     // Setup listener for slider events.
     $scope.$sliderYaw = $('.slider-yaw').slider()
     .on('slide', function (event) {
-
         // Update the UI for current slider value.
         $scope.$apply($scope.sliderYaw = event.value);
-
-        // Send the value over the WS connection.
-        if (ws.readyState === 1)
-            ws.send("yaw:"+event.value);
     })
     .on("slideStop", function () {
       $scope.$apply(resetYaw);
@@ -56,13 +80,8 @@ function WebSocketController($scope) {
 
     $scope.$sliderRoll = $('.slider-roll').slider()
     .on('slide', function (event) {
-
         // Update the UI for current slider value.
         $scope.$apply($scope.sliderRoll = event.value);
-
-        // Send the value over the WS connection.
-        if (ws.readyState === 1)
-            ws.send("roll:"+event.value);
     })
     .on("slideStop", function () {
       $scope.$apply(resetRoll);
@@ -70,13 +89,8 @@ function WebSocketController($scope) {
 
     $scope.$sliderPitch = $(".slider-pitch").slider()
     .on('slide', function (event) {
-
         // Update the UI for current slider value.
         $scope.$apply($scope.sliderPitch = event.value);
-
-        // Send the value over the WS connection.
-        if (ws.readyState === 1)
-            ws.send("pitch:"+event.value);
     })
     .on("slideStop", function () {
       $scope.$apply(resetPitch);
